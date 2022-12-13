ee9e0b6978b9c44aa76404296f4f5c881736c280
JoseLuisPovedanoPoyato <joseluis112000@hotmail.es>
2022-12-13 19:31:53 +0100

    Print process output

--- ApacheTimeRecording/run_requests.py
+++ ApacheTimeRecording/run_requests.py
@@ -11,3 +11,4 @@ print(requests)
 # Read request data and execute them
 for req_path in requests:
     process = subprocess.run(['ab', '-p', req_path, '-T', 'application/json', '-c', '1', '-n', '10', '-v', '4', 'http://micro-counter-service/count'], capture_output=True, text=True)
+    print(process)
