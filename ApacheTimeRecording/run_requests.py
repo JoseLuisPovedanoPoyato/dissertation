f115b5f90a0077fe4a930075c68d0bfa0f169a9a
JoseLuisPovedanoPoyato <joseluis112000@hotmail.es>
2022-12-13 21:42:09 +0100

    Testing extracting times

--- ApacheTimeRecording/run_requests.py
+++ ApacheTimeRecording/run_requests.py
@@ -1,6 +1,6 @@
 import subprocess
 from pathlib import Path
-
+import re
 
 # Get requests
 file_path = Path.cwd()
@@ -8,7 +8,16 @@ requests = [str(f) for f in list(file_path.glob('**/*.json'))]
 
 print(requests)
 
+results = {}
+
 # Read request data and execute them
-for req_path in requests:
+for i in range(len(requests)):
+    req_path = requests[i]
     process = subprocess.run(['ab', '-p', req_path, '-T', 'application/json', '-c', '1', '-n', '10', '-v', '4', 'http://micro-counter-service/count'], capture_output=True, text=True)
     print(process)
+    logs = process.stdout
+
+    result = re.search('Time per request:(.*)\n', logs)
+    print(result)
+    print(result.group(1))
+    results[i] = ["", ""]
