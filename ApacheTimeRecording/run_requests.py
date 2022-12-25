2eabfd4a87ecbc02349c9fcfe1c00fe4a41c169a
JoseLuisPovedanoPoyato <joseluis112000@hotmail.es>
2022-12-25 15:38:12 +0100

    Reset normal requests

--- ApacheTimeRecording/run_requests.py
+++ ApacheTimeRecording/run_requests.py
@@ -4,13 +4,13 @@ import re
 import json
 
 # We simulate 1, 5, 10, 100 and 1000 users sending simultaneous requests
-CONCURRENT_REQUESTS=[1, 5, 10, 100]
+CONCURRENT_REQUESTS=[1, 5, 10, 100, 1000]
 
 # Each user will try to send 1, 2, 4, 8 and 16 requests each
 APACHE_CONC_NUM_COMBINATIONS = 0 
 NUM_OF_REQUESTS = {}
 for conc in CONCURRENT_REQUESTS:
-    NUM_OF_REQUESTS[conc]=[conc, conc * 2, conc * 4, conc * 8]
+    NUM_OF_REQUESTS[conc]=[conc, conc * 2, conc * 4, conc * 8, conc * 16]
     APACHE_CONC_NUM_COMBINATIONS += 5
 
 
@@ -38,7 +38,7 @@ def send_requests():
                 count += 1
                 print(f"SENDING REQUEST:     {count}/{max_value}")
                 req_path = requests[i]
-                process = subprocess.run(['ab', '-p', req_path, '-T', 'application/json', '-c', str(concurrency), '-n', str(num), '-v', '1', '-s', '60', 'http://micro-counter-service/count'], capture_output=True, text=True)
+                process = subprocess.run(['ab', '-p', req_path, '-T', 'application/json', '-c', str(concurrency), '-n', str(num), '-v', '1', '-s', '90', 'http://micro-counter-service/count'], capture_output=True, text=True)
                 logs = process.stdout
                 errors = process.stderr
                 print(logs)
