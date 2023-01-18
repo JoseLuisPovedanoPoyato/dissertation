66bae0d0b37977a69ce34630d2a569eebc5f4864
Jose Luis Povedano Poyato <joseluis112000@hotmail.es>
2023-01-18 20:19:47 +0100

    Updated docker files

---
+++ Jobs/create_docker_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function create_request_generator(){
+	docker rmi requestgenerator:1.0
+	docker build . -t requestgenerator:1.0 -f ../RequestGenerator/Dockerfile
+	kind load docker-image requestgenerator:1.0
+}
+
+function create_request_generator(){
+	docker rmi benchmarkcontroller:1.0
+	docker build . -t benchmarkcontroller:1.0 -f ../BenchmarkController/Dockerfile
+	kind load docker-image requestgenerator:1.0
+}
+
+function create_docker_images(){
+	create_request_generator
+	create_benchmark_controller
+}
+# --
+
+if [ "$(basename $0)" = "create_docker_images.sh" ] ; then
+	create_docker_images
+fi
