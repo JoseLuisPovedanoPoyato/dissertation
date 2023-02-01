9f544f60d04f4c88d23678abdb661969a612b7fe
Jose Luis Povedano Poyato <joseluis112000@hotmail.es>
2023-02-01 15:44:50 +0100

    Update ci.sh

--- Jobs/ci.sh
+++ Jobs/ci.sh
@@ -15,20 +15,28 @@ git pull origin main
 if git diff HEAD^ HEAD --name-only | grep -E "(Dockerfile|\.py)" | grep "../MicroCounter/" > /dev/null
 then
   create_micro_counter_img
+else
+  echo "No Updates to MicroCounter Docker or Python"
 fi
 # Check if BenchmarkController DockerFiles or Python Files have changed
 if git diff HEAD^ HEAD --name-only | grep -E "(Dockerfile|\.py)" | grep "../BenchmarkController/" > /dev/null
 then
   create_benchmark_controller_img
+else
+  echo "No Updates to BenchmarkController Docker or Python"
 fi
 # Check if RequestGenerator DockerFiles or Python Files have changed
 if git diff HEAD^ HEAD --name-only | grep -E "(Dockerfile|\.py)" | grep "../RequestGenerator/" > /dev/null
 then
   create_request_generator_img
+else
+  echo "No Updates to RequestGenerator Docker or Python"
 fi
 
 # Check for Prometheus updates
 if git diff HEAD^ HEAD --name-only | grep ".yaml" | grep "../PrometheusService/" > /dev/null
 then
-    kubectl apply -f ../PrometheusService/
+  kubectl apply -f ../PrometheusService/
+else
+  echo "No Updates to Prometheus Service"
 fi
