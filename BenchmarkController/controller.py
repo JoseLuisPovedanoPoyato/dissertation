39312bd3521e6783aca09eba80b7ee44374d7615
Jose Luis Povedano Poyato <joseluis112000@hotmail.es>
2023-02-06 17:36:38 +0100

    Lowering resource usage limits, reducing values in user data

--- BenchmarkController/controller.py
+++ BenchmarkController/controller.py
@@ -7,9 +7,9 @@ import pathlib
 ### Default Values for unspecified benchmark runs ###
 # We simulate 1, 5, 10, (and maybe 100 and 1000) users sending simultaneous requests
 # Each user will try to send 1, 2, 4, 8 and 16 requests each
-CONCURRENT_USERS=[100, 500, 1000, 1500] # Increase the number of users
+CONCURRENT_USERS=[100, 200, 400, 600, 800, 1000] # Increase the number of users
 REQUESTS_PER_USER = [1] #2, 4, 8, 16
-SERVICES_PER_REQ = [10, 100, 200, 300, 400] #Scale down microservices
+SERVICES_PER_REQ = [10, 100, 200, 300] #Scale down microservices
 
 REQUEST_GENERATOR_URL = "http://request-generator-service/generate_load"
 
