012cc2d4493d1c613c06dca9c83c440153b2e77c
Jose Luis Povedano Poyato <joseluis112000@hotmail.es>
2023-02-08 19:03:03 +0100

    Update controller.py

--- BenchmarkController/controller.py
+++ BenchmarkController/controller.py
@@ -9,7 +9,7 @@ import pathlib
 # Each user will try to send 1, 2, 4, 8 and 16 requests each
 CONCURRENT_USERS=[100, 200, 400, 600, 800, 1000]
 REQUESTS_PER_USER = [1] #2, 4, 8, 16
-SERVICES_PER_REQ = [10, 50, 100, 150, 200]
+SERVICES_PER_REQ = [10, 20, 40, 60, 80, 100]
 
 REQUEST_GENERATOR_URL = "http://request-generator-service/generate_load"
 
