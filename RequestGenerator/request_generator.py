8e7d70a124ea33716dd13eef7a9eb750b796b995
Jose Luis Povedano Poyato <joseluis112000@hotmail.es>
2023-02-11 19:48:23 +0100

    Update request_generator.py

--- RequestGenerator/request_generator.py
+++ RequestGenerator/request_generator.py
@@ -135,6 +135,8 @@ def gather_resource_metrics(start, memory_file, cpu_file):
 def group_2d_list_by_repeated_first_element(list_2d):
     d = {l[0]: 0 for l in list_2d}
     for l in list_2d:
+        print(l[0])
+        print(l[1])
         d[l[0]] += float(l[1])
     return list(map(tuple, d.items()))
 
