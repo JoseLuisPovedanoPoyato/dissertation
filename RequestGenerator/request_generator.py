2674a65d655bedabae114e5ccaf264c87dd1c210
Jose Luis Povedano Poyato <joseluis112000@hotmail.es>
2023-03-02 13:37:29 +0000

    Update request_generator.py

--- RequestGenerator/request_generator.py
+++ RequestGenerator/request_generator.py
@@ -22,8 +22,8 @@ def run_apache_request(user, request, service, post_file, results_dir):
     cpu_file = f"{results_dir}/cpu_{user}"
     cpu_data_plane_file = f"{results_dir}/cpu_data_plane_{user}"
     cpu_control_plane_file = f"{results_dir}/cpu_control_plane_{user}"
-    mem_data_plane_file = f"{results_dir}/mem_data_plane_{user}_{request}_{service}"
-    mem_control_plane_file = f"{results_dir}/mem_control_plane_{user}_{request}_{service}"
+    mem_data_plane_file = f"{results_dir}/mem_data_plane_{user}"
+    mem_control_plane_file = f"{results_dir}/mem_control_plane_{user}"
     log_files(csv_file, gnu_file, memory_file, cpu_file, cpu_data_plane_file, cpu_control_plane_file, mem_data_plane_file, mem_control_plane_file)
     start = time.time()
     process = subprocess.run(['ab', '-p', post_file, '-T', 'application/json', '-c', str(user), '-n', str(request * user), '-e', csv_file, '-g', gnu_file, '-v', '1', '-s', '300', micro_counter_url], capture_output=True, text=True)
